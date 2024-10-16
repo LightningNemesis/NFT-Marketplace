8c5c75092f2925ee821f6fc82d0525059c2fed65
LightningNemesis <ak.kumar220919@gmail.com>
2024-10-16 07:03:37 -0700

    Stable contract, need to test Royalty

--- contracts/NFTMarketplaceUpdated.sol
+++ contracts/NFTMarketplaceUpdated.sol
@@ -16,6 +16,9 @@ contract MyNFTMarketplace is ERC721, Ownable {
 
     mapping(uint256 => string) private _tokenURIs;
     mapping(uint256 => Listing) private _listings;
+    mapping(uint256 => address) private _creators;
+    mapping(uint256 => uint256) private _royalties;
+    mapping(address => uint256[]) private _ownedTokens; // Track owned tokens
 
     event NFTListed(uint256 tokenId, uint256 price, address seller);
     event NFTSold(
@@ -27,11 +30,19 @@ contract MyNFTMarketplace is ERC721, Ownable {
 
     constructor() ERC721("MyNFT", "MNFT") {}
 
-    function mint(string memory _tokenURI) public returns (uint256) {
+    function mint(
+        string memory _tokenURI,
+        uint256 royalty
+    ) public returns (uint256) {
+        require(royalty <= 100, "Royalty must be between 0 and 100");
+
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, _tokenURI);
+        _creators[newItemId] = msg.sender;
+        _royalties[newItemId] = royalty;
+        _ownedTokens[msg.sender].push(newItemId); // Track ownership
         return newItemId;
     }
 
@@ -52,15 +63,32 @@ contract MyNFTMarketplace is ERC721, Ownable {
         address seller = listing.seller;
         uint256 price = listing.price;
 
+        // Calculate royalty
+        uint256 royaltyAmount = (price * _royalties[tokenId]) / 100;
+
+        // Pay seller and creator
+        payable(seller).transfer(price - royaltyAmount);
+        payable(_creators[tokenId]).transfer(royaltyAmount);
+
         delete _listings[tokenId];
+
+        // Update ownership tracking
+        removeTokenFromOwner(seller, tokenId);
+        _ownedTokens[msg.sender].push(tokenId);
+
         _transfer(seller, msg.sender, tokenId);
-        payable(seller).transfer(price);
 
         emit NFTSold(tokenId, price, seller, msg.sender);
 
         if (msg.value > price) {
             payable(msg.sender).transfer(msg.value - price);
         }
+    }
+
+    function getOwnedTokens(
+        address owner
+    ) public view returns (uint256[] memory) {
+        return _ownedTokens[owner];
     }
 
     function getListedNFTs() public view returns (uint256[] memory) {
@@ -83,11 +111,31 @@ contract MyNFTMarketplace is ERC721, Ownable {
         return listedNFTs;
     }
 
+    function removeTokenFromOwner(address owner, uint256 tokenId) internal {
+        uint256 length = _ownedTokens[owner].length;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (_ownedTokens[owner][i] == tokenId) {
+                _ownedTokens[owner][i] = _ownedTokens[owner][length - 1];
+                _ownedTokens[owner].pop();
+                break;
+            }
+        }
+    }
+
     function getNFTListing(
         uint256 tokenId
     ) public view returns (uint256 price, address seller) {
         Listing memory listing = _listings[tokenId];
         return (listing.price, listing.seller);
+    }
+
+    function getCreator(uint256 tokenId) public view returns (address) {
+        return _creators[tokenId];
+    }
+
+    function getRoyalty(uint256 tokenId) public view returns (uint256) {
+        return _royalties[tokenId];
     }
 
     function _setTokenURI(
@@ -113,10 +161,12 @@ contract MyNFTMarketplace is ERC721, Ownable {
 
     function _burn(uint256 tokenId) internal virtual override {
         super._burn(tokenId);
+
         if (bytes(_tokenURIs[tokenId]).length != 0) {
             delete _tokenURIs[tokenId];
+            delete _creators[tokenId];
+            delete _royalties[tokenId];
+            removeTokenFromOwner(ownerOf(tokenId), tokenId); // Remove from tracking
         }
     }
-
-    // Existing functions (_setTokenURI, tokenURI, _burn) remain the same
 }
